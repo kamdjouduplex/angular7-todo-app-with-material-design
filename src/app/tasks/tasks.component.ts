45b7f1747c752b8dbe1542b1beffd9a604c602b4
kamdjouduplex <tony14pro@gmail.com>
2019-01-05 11:07:39 +0100

    app finished

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -18,28 +18,8 @@ export class TasksComponent implements OnInit {
 
   todos: any[] = this.data.getTodos();
 
-  // folders: Section[] = [
-  //   {
-  //     name: 'Photos',
-  //     updated: new Date('1/1/16'),
-  //   },
-  //   {
-  //     name: 'Recipes',
-  //     updated: new Date('1/17/16'),
-  //   },
-  //   {
-  //     name: 'Work',
-  //     updated: new Date('1/28/16'),
-  //   }
-  // ];
-  // notes: Section[] = [
-  //   {
-  //     name: 'Vacation Itinerary',
-  //     updated: new Date('2/20/16'),
-  //   },
-  //   {
-  //     name: 'Kitchen Remodel',
-  //     updated: new Date('1/18/16'),
-  //   }
-  // ];
+  deleteItem(id){
+    this.data.deleteTodo(id);
+    return this.todos;
+  }
 }
--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -16,4 +16,12 @@ export class TodoService {
   addTodo(id:number, title:string){
     return this.todos.push({id: id, title: title});
   }
+
+  deleteTodo(id:number){
+    let index = id-1;
+    if (index > -1) {
+      this.todos.splice(index, 1);
+    }
+    return this.todos;
+  }
 }
